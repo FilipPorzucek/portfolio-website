9eb9b78f91772824c2437893ecc714f35691818e
FilipPorzucek <filipporzucek81@gmail.com>
2024-10-07 17:18:30 +0200

    Poprawa responsywnosci strony

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -49,17 +49,17 @@ import { Url } from 'url';
    <img class="sm:w-36 w-16 mb-12 ml-2 object-cover fade-in" src="assets/certificate.png">
    <mat-icon  class="mat-icon -mt-12 sm:-mt-8 fade-in">get_app</mat-icon>
   </div>
-  <p class=" text-gray-500 sm:ml-16 mt-3 sm:mt-6 font-serif fade-in sm:text-xl text-base " >JAVA DEVELOPER</p>
-  <p class="w-48 text-sm sm:text-m  sm:ml-14 text-gray-400 text-center fade-in mb-4 sm:mb-0">certificate of completion of the java developer course</p>
+  <p class=" text-gray-500 sm:ml-2 mt-3 sm:mt-6 font-serif fade-in sm:text-xl text-base " >JAVA DEVELOPER</p>
+  <p class="w-48 text-sm sm:text-m  sm:ml-4 text-gray-400 text-center fade-in mb-4 sm:mb-0">certificate of completion of the java developer course</p>
   </div>
 
   <div class="flex flex-col items-center justify-center">
-  <div class="circle flex flex-col items-center justify-center overflow-hidden fade-in">
+  <div class="circle flex flex-col items-center justify-center sm:mt-9 overflow-hidden fade-in">
   <img class="sm:w-36 w-16 mb-12 ml-2 object-cover fade-in" src="assets/certificate.png">
    <mat-icon  class="mat-icon -mt-12 sm:-mt-8 fade-in" (click)="downloadPdf(const2)">get_app</mat-icon>
   </div>
-  <p class=" sm:text-xl text-base text-gray-500 sm:ml-12 mt-3 sm:mt-6 font-serif w-56 text-center fade-in">GRAPHIC AND DIGITAL PUBLISHING TECHNICANS</p>
-  <p class="w-52 text-m   sm:ml-14 text-gray-400 text-center fade-in mb-4 sm:mb-0">certificate of completion of the java developer course</p>
+  <p class=" sm:text-xl text-base text-gray-500 sm:ml-8 mt-3 sm:mt-6 font-serif w-56 text-center fade-in">GRAPHIC AND DIGITAL PUBLISHING TECHNICANS</p>
+  <p class="w-52 text-m   sm:ml-10 text-gray-400 text-center fade-in mb-4 sm:mb-0">certificate of completion of the java developer course</p>
   </div>
 
     <div class="flex flex-col items-center justify-center">
@@ -67,8 +67,8 @@ import { Url } from 'url';
     <img class="sm:w-36 w-16 mb-12 ml-2 object-cover fade-in" src="assets/certificate.png">
   <mat-icon  class="mat-icon -mt-12 sm:-mt-8 fade-in" (click)="downloadPdf(const1)" >get_app</mat-icon>
   </div>
-  <p class="text-gray-500 sm:ml-16 mt-3 sm:mt-6 font-serif fade-in sm:text-xl text-base ">GALLUP CERTIFICATE</p>
-  <p class="w-48  text-sm sm:text-m  sm:ml-14 text-gray-400 text-center fade-in">a test showing my best predispositions and soft skills</p>
+  <p class="text-gray-500 sm:ml-8 mt-3 sm:mt-6 font-serif fade-in sm:text-xl text-base ">GALLUP CERTIFICATE</p>
+  <p class="w-48  text-sm sm:text-m  sm:ml-10 text-gray-400 text-center fade-in">a test showing my best predispositions and soft skills</p>
   </div>
   </div>
 
--- src/app/home/home-about-section.component.ts
+++ src/app/home/home-about-section.component.ts
@@ -7,11 +7,13 @@ import { AnimationService } from '../animation.service';
   imports: [],
   template: `<div id="section1" class="mt-48  sm:mt-40 bg-gray-400 fade-in pb-16 ">
   <p  class="fade-in font-serif float-left text-3xl sm:text-5xl  ml-8 sm:ml-28  mt-6 text-gray-600 mb-8">about.</p>
-  <p   class=" fade-in clear-left text-left ml-20 sm:ml-36 font-serif text-lg sm:text-2xl  w-3/4    text-white">My name is Filip and I am currently a computer science student in Poznań. My greatest passion is programming. I started by creating simple websites in html and css. Then I learned javascript and typescript, currently I create my projects in the Angular framework and 
-    I spend a lot of time learning Java. 
-    I created designs or graphics for my websites myself in Adobe programs because I have the title of graphic technician. 
-    I already have my first experience in programming because I participated in creating a website for a certain store. I also have other projects. I have been interested in programming for over two years. I am a communicative, creative person and I like working in a team. I avoid conflicts and always look for a solution to a given problem.
-     Apart from programming, I do a lot of sports such as running or long hikes. I also like reading biographies..</p>
+  <p   class=" fade-in clear-left text-left ml-20 sm:ml-36 font-serif text-lg sm:text-2xl  w-3/4    text-white">My name is Filip and I am currently a computer science student in Poznań. My greatest passion is programming. 
+    I started by creating simple websites in html and css. Then I learned javascript and typescript, currently I create my projects in the Angular framework and 
+  My name is Filip and I am currently studying computer science in Poznań. My greatest passion is programming. I started by creating simple websites in html and css.
+   Then I learned javascript and typescript, currently I create my projects in the Angular framework and I spend a lot of time learning Java. 
+  I created projects or graphics for my websites myself in Adobe programs, because I have the title of graphic designer. I already have my first experience in programming, because I participated in creating a website for a certain store.
+   I also have other projects that you can see in the projects tab. I am a communicative, creative person and I like working in a team. 
+  I avoid conflicts and always look for a solution to a given problem. Apart from programming, I do many sports, such as running or long hikes. I also like reading biographies.</p>
    </div> ` 
 })
 export class HomeAboutSectionComponent {
